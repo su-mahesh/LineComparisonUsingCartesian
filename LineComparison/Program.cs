42e1267853489c23986fa0341bcaa0250a326c74
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-04 19:58:27 +0530

    [MaheshKangude] REFACTORED: used class and methods

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -2,75 +2,94 @@
 
 namespace LineComparison
 {
-    class LineComparison
+    class Line
     {
         private int x1;
         private int x2;
         private int y1;
         private int y2;
-                
-        public void GetEndPoints() {
-            InputPoint:
-            Console.WriteLine("Enter line points x1 and y1 points(x1 y1): ");
-            try
-            {
-                String[] input = Console.ReadLine().Split(" ");
-                x1 = Convert.ToInt32(input[0]);
-                y1 = Convert.ToInt32(input[1]);
+        private float Length;
 
-                Console.WriteLine("Enter line points x2 and y2 points(x2 y2): ");
-                input = Console.ReadLine().Split(" ");
-                x2 = Convert.ToInt32(input[0]);
-                y2 = Convert.ToInt32(input[1]);
-            }
-            catch (Exception) {
-                Console.WriteLine("wrong input");
-                goto InputPoint;
-            }
+        public Line(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
         }
 
-        public float CalculateLength() {
-          
-            float LineLength = (float)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("Line length: "+LineLength);
+        public int GetX1()
+        {
+            return x1;
+        }
+        public int GetX2()
+        {
+            return x2;
+        }
+        public int GetY1()
+        {
+            return y1;
+        }
+        public int GetY2()
+        {
+            return y2;
+        }
+        public void SetLength(float Length) {
+            this.Length = Length;
+        }
+        public override bool Equals(Object line) {
+            return this.Length.Equals(((Line)line).Length);
+        }
+
+        public int CompareTo(Line line)
+        {       
+            return this.Length.CompareTo(line.Length);
+        }
+
+    }  
+    class LineComparison
+    {
+        public float CalculateLineLength(Line line) {
+            float LineLength = (float)Math.Sqrt(Math.Pow((line.GetX2() - line.GetX1()), 2) + Math.Pow((line.GetY1() - line.GetY2()), 2));
+            Console.WriteLine("Line length: "+ LineLength +"\n");
+            line.SetLength(LineLength);
             return LineLength;
         }
-        private void CheckEqualLines() {
-            GetEndPoints();
-            float LineLenght1 = CalculateLength();
-            GetEndPoints();
-            float LineLenght2 = CalculateLength();
-
-            if (LineLenght1.Equals(LineLenght2))
-                Console.WriteLine("lines are equals");
+        private void CheckEqualLines(Line line1, Line line2) {
+            Console.WriteLine("Line 1:");
+            float LineLenght1 = CalculateLineLength(line1);
+            Console.WriteLine("Line 2");
+            float LineLenght2 = CalculateLineLength(line2);
+            
+            if (line1.Equals(line2))
+                Console.WriteLine("lines are equals\n");
             else
-                Console.WriteLine("lines are not equal");
+                Console.WriteLine("lines are not equal\n");
         }
-        private void CompareLines()
+        private void CompareLines(Line line1, Line line2)
         {
-            Console.WriteLine("\nLine 1");
-            GetEndPoints();
-            float LineLenght1 = CalculateLength();
-            Console.WriteLine("\nLine 2");
-            GetEndPoints();
-            float LineLenght2 = CalculateLength();
+            Console.WriteLine("Line 1:");
+            float LineLenght1 = CalculateLineLength(line1);
+            Console.WriteLine("Line 2");
+            float LineLenght2 = CalculateLineLength(line2);
 
-            if (LineLenght1.CompareTo(LineLenght2) == 0)
-                Console.WriteLine("both lines are equal");
+            if (line1.CompareTo(line2) == 0)
+                Console.WriteLine("both lines are equal\n");
             else if (LineLenght1.CompareTo(LineLenght2) > 0)
-                Console.WriteLine("first line is bigger than second");
+                Console.WriteLine("first line is bigger than second\n");
             else
-                Console.WriteLine("first line is smaller than second");
+                Console.WriteLine("first line is smaller than second\n");
         }
         static void Main(string[] args)
         {
             LineComparison lineComparison = new LineComparison();
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-        
-            lineComparison.GetEndPoints();
-            lineComparison.CalculateLength();
-            lineComparison.CheckEqualLines();
-            lineComparison.CompareLines();
+            Line line1 = new Line(4, 0, 1, 1);
+            Line line2 = new Line(1, 1, 0, 10);
+            lineComparison.CalculateLineLength(line1);
+            lineComparison.CalculateLineLength(line2);
+            lineComparison.CheckEqualLines(line1, line2);
+            lineComparison.CompareLines(line1, line2);
         }      
     }
 }
